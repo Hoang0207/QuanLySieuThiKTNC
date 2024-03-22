9619dedc4c0fb806ae12fff1b6becb4bf7984868
water-1327 <140366167+water-1327@users.noreply.github.com>
2024-03-22 11:01:52 +0700

    nhanvienbanhang(1)

--- src/test/java/com/supermarket/UI/BillThanhToanFrameTest.java
+++ src/test/java/com/supermarket/UI/BillThanhToanFrameTest.java
@@ -1,20 +1,158 @@
 package com.supermarket.UI;
 
 import org.testng.annotations.Test;
+
+import com.supermarket.ENTITY.BillThanhToan;
+
 import org.testng.annotations.BeforeMethod;
+
+import static org.junit.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.List;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
 public class BillThanhToanFrameTest {
+	public BillThanhToanFrame framebill;
+	private Robot robot;
+	private Thread t1,t2,t3;
+	
+  @BeforeTest
+  public static void beforeClass() throws AWTException{
+	  System.out.println("Start Test");
+  }
 
   @BeforeMethod
-  public void beforeMethod() {
+  public void beforeMethod() throws AWTException {
+	  framebill = new BillThanhToanFrame(null, false);
+	  robot = new Robot();
+	  System.out.println("Starting test");
   }
 
+  
+  @Test
+  public void getDataBill() throws Exception{
+	  t1 = new Thread() {
+		  @Override
+		  public void run() {
+			  framebill.setVisible(true);
+		  }
+	  };
+	  t2= new Thread() {
+		  @Override
+		  public void run() {
+			  try {
+				  List<BillThanhToan> list = framebill.billThanhToan;
+				  if(list.size()<=0) {
+					  fail("Not Data");
+				  }
+				  else {
+					  assertTrue(list.size()>1);
+					  System.out.println("Pass");
+				  }
+			  }
+			  catch(Exception e) {
+				  e.printStackTrace();
+			  }
+			  
+		  }
+	  };
+	  t1.start();
+	  t1.sleep(3000);
+	  t2.start();
+	  t2.sleep(3000);
+	  
+  }
+  
+  @Test
+  public void getDataBill1() throws Exception{
+	  t1 = new Thread() {
+		  @Override
+		  public void run() {
+			  framebill.setVisible(true);
+		  }
+	  };
+	  t2= new Thread() {
+		  @Override
+		  public void run() {
+			  try {
+				  List<BillThanhToan> list = framebill.billThanhToan;
+				  if(list.size()<=0) {
+					  fail("Not Data");
+				  }
+				  else {
+					  assertTrue(list.size()>1);
+					  System.out.println("Pass");
+				  }
+			  }
+			  catch(Exception e) {
+				  e.printStackTrace();
+			  }
+			  
+		  }
+	  };
+	  t1.start();
+	  t1.sleep(3000);
+	  t2.start();
+	  t2.sleep(3000);
+	  
+  }
+  
+  @Test
+  public void getDataBill2() throws Exception{
+	  t1 = new Thread() {
+		  @Override
+		  public void run() {
+			  framebill.setVisible(true);
+		  }
+	  };
+	  t2= new Thread() {
+		  @Override
+		  public void run() {
+			  try {
+				  List<BillThanhToan> list = framebill.billThanhToan;
+				  if(list.size()<=0) {
+					  fail("Not Data");
+				  }
+				  else {
+					  assertTrue(list.size()>1);
+					  System.out.println("Pass");
+				  }
+			  }
+			  catch(Exception e) {
+				  e.printStackTrace();
+			  }
+			  
+		  }
+	  };
+	  t1.start();
+	  t1.sleep(3000);
+	  t2.start();
+	  t2.sleep(3000);
+	  
+  }
+  
+  
+  
+  
+  
   @AfterMethod
   public void afterMethod() {
+	  framebill.setVisible(false);
+	  framebill = null;
+	  t1=null;
+	  t2=null;
+	  t3=null;
+	  System.out.println("Finish");
   }
+  
+  
 
   @BeforeTest
   public void beforeTest() {
@@ -22,6 +160,7 @@ public class BillThanhToanFrameTest {
 
   @AfterTest
   public void afterTest() {
+	  System.out.println("Finish Test");
   }
 
 }
--- src/test/java/com/supermarket/UI/NhanVienBanHangTest.java
+++ src/test/java/com/supermarket/UI/NhanVienBanHangTest.java
@@ -1,27 +1,135 @@
 package com.supermarket.UI;
 
 import org.testng.annotations.Test;
+
+import com.supermarket.ENTITY.BillThanhToan;
+import com.supermarket.ENTITY.SanPham;
+
 import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.fail;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.Arrays;
+
+import javax.swing.JLabel;
+
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
 public class NhanVienBanHangTest {
+	NhanVienBanHang nvbh;
+	private Robot robot;
+	Thread t1,t2,t3;
  
   @BeforeMethod
-  public void beforeMethod() {
+  public void beforeMethod() throws Exception {
+	  nvbh = new NhanVienBanHang();
+	  robot = new Robot();
+	  System.out.println("Start test");
   }
-
+  
+  
+  @Test
+  public void btnAddBill() throws InterruptedException {
+	  t1= new Thread() {
+		  @Override
+		  public void run() {
+			  nvbh.setVisible(true);
+		  }
+	  };
+	  t2= new Thread() {
+		  @Override
+		  public void run() {
+			  nvbh.getTxtSoLuong().setText("4");
+			  try {sleep(1000);}catch(Exception e) {e.printStackTrace();}
+			  nvbh.getTxtTenSP().setText("abc");
+			  try {sleep(1000);}catch(Exception e) {e.printStackTrace();}
+			  nvbh.getBtnThemHD();
+			  try {sleep(1000);}catch(Exception e) {e.printStackTrace();}
+		  } 
+	  };
+	  t3 = new Thread() {
+		@Override
+		public void run() {
+			try {
+				nvbh.getBtnThemHD().doClick();
+				Assert.fail("KHong du doan duoc");
+			}catch(Exception e) {
+				System.out.println("Dự đoán trước: sai tt");
+			};
+			
+		}
+	  };
+	  t1.sleep(1000);
+	  t1.start();
+	  t2.sleep(3000);
+	  t2.start();
+	  t3.sleep(6000);
+	  t3.start();
+  }
+  
+  @Test
+  public void btnAddBill1() throws InterruptedException {
+	  t1= new Thread() {
+		  @Override
+		  public void run() {
+			  nvbh.setVisible(true);
+		  }
+	  };
+	  t2= new Thread() {
+		  @Override
+		  public void run() {
+			  nvbh.getTxtSoLuong().setText("10");
+			  try {sleep(1000);}catch(Exception e) {e.printStackTrace();}
+			  nvbh.getTxtTenSP().setText("MILO");
+			  try {sleep(1000);}catch(Exception e) {e.printStackTrace();}
+			  nvbh.getBtnThemHD();
+			  try {sleep(1000);}catch(Exception e) {e.printStackTrace();}
+		  } 
+	  };
+	  t3 = new Thread() {
+		@Override
+		public void run() {
+			try {
+				nvbh.getBtnThemHD().doClick();
+				Assert.fail("KHong du doan duoc");
+			}catch(Exception e) {
+				System.out.println("Dự đoán trước: sai tt");
+			};
+			
+		}
+	  };
+	  t1.sleep(1000);
+	  t1.start();
+	  t2.sleep(3000);
+	  t2.start();
+	  t3.sleep(6000);
+	  t3.start();
+  }
+  
+  
+  
+  
   @AfterMethod
   public void afterMethod() {
+	  nvbh.setVisible(false);
+	  nvbh= null;
+	  t1=null;
+	  t2=null;
+	  t3=null;
+	  System.out.println("Finish");
   }
+  
+  
 
-  @BeforeTest
-  public void beforeTest() {
-  }
 
   @AfterTest
   public void afterTest() {
+	  System.out.println("Finish Test");
   }
 
 }
